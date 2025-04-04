5851a875c1c742af06b51a89e55dc9d54dc20f19
kevin <ejykken@gmail.com>
2025-04-05 00:04:07 +0100

    Add first version of mailtransport client package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mailtransport",
+    version="0.1.0",
+    author="mailtransportai",
+    author_email="support@mailtransportai.com",
+    description="A Python package for managing email transport.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mailtransportai/mailtransport-py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
